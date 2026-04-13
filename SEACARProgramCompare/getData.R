8a645af9ba0f9d45e3690fa5bb949844fdc41d15
Tylar <murray.tylar@gmail.com>
2026-04-13 16:43:30 -0400

    fix mappings

--- SEACARProgramCompare/getData.R
+++ SEACARProgramCompare/getData.R
@@ -1,5 +1,5 @@
 library(here)
-source(here("SEACARProgramCompare/mapSEACARProgramNameToShortName.R"))
+source(here("SEACARProgramCompare/mapProgramNameToShortName.R"))
 
 # Reads unique programs from both datasetsand returns them as a list
 getData <- function(batch_value) {
@@ -11,14 +11,15 @@ getData <- function(batch_value) {
   ) %>%
   # Convert ActivityDepth_m to numeric for plotting
   mutate(ActivityDepth_m = as.numeric(ActivityDepth_m)) %>%
-  mutate(ProgramName = mapSEACARProgramNameToShortName(ProgramName))
+  mutate(ProgramName = mapProgramNameToShortName(ProgramName))
 
   df2 <- read.csv(here::here("data/allDataSEACAR.csv")) %>% 
   # Align column types with df1:
   # Convert logical columns to character (df1 has these as character)
   mutate(across(where(is.logical), as.character)) %>%
   # Convert SampleDate to datetime to match df1 format
-  mutate(SampleDate = as.POSIXct(SampleDate, format = "%Y-%m-%d", tz = "UTC"))
+  mutate(SampleDate = as.POSIXct(SampleDate, format = "%Y-%m-%d", tz = "UTC")) %>%
+  mutate(ProgramName = mapProgramNameToShortName(ProgramName))
   
 
   df1 <- df1[df1$ProgramName == batch_value, ]
--- SEACARProgramCompare/getListOfValues.R
+++ SEACARProgramCompare/getListOfValues.R
@@ -1,15 +1,19 @@
 library(here)
-source(here("SEACARProgramCompare/mapSEACARProgramNameToShortName.R"))
+source(here("SEACARProgramCompare/mapProgramNameToShortName.R"))
 getListOfValues <- function() {
   df1 <- readr::read_delim(
     here("data/Discrete WQ - 10006.txt"),
     delim = "|"
-  ) %>% dplyr::mutate(
-    ProgramName = mapSEACARProgramNameToShortName(ProgramName)
+  ) %>% 
+  dplyr::mutate(
+    ProgramName = sapply(ProgramName, mapProgramNameToShortName)
   )
   unique_df1 <- unique(df1$ProgramName)
 
-  df2 <- read.csv(here::here("data/allDataSEACAR.csv"))
+  df2 <- read.csv(here::here("data/allDataSEACAR.csv")) %>% 
+  dplyr::mutate(
+    ProgramName = sapply(ProgramName, mapProgramNameToShortName)
+  )
   unique_df2 <- unique(df2$ProgramName)
   
   # merge the two unique lists
